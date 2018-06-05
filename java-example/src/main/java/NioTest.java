f4d944b3e080613f501b74e21ee367766de3cc1b
xlp <xionglinpeng@163.com>
2018-06-05 09:22:08 +0800

    nio

--- java-example/src/main/java/NioTest.java
+++ java-example/src/main/java/NioTest.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  * Created by haolw on 2018/5/29.
  */
 public class NioTest {
+
+    public static void main(String[] args) throws Exception{
+
+        byte[] message = "hello nio .".getBytes();
+
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\lenovo\\Desktop\\nio.txt");
+
+        FileChannel fileChannel = fos.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        for(byte msg : message){
+            byteBuffer.put(msg);
+        }
+
+        System.out.println();
+
+        byteBuffer.flip();
+
+        fileChannel.write(byteBuffer);
+
+    }
 }
--- java-example/src/main/java/thread/pool/Test.java
+++ java-example/src/main/java/thread/pool/Test.java
@@ -4,4 +4,37 @@ package thread.pool;
  * Created by haolw on 2018/5/31.
  */
 public class Test {
+
+    public static void main(String[] args) {
+
+//        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(10);
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(1,4,1, TimeUnit.DAYS,queue);
+//        for(int i = 0 ;i < 10;i++){
+//            final int j = i;
+//            executor.execute(new Runnable() {
+//                public void run() {
+//                    try {
+//                        System.out.println(new Random().nextInt(10000));
+//                        Thread.sleep(new Random().nextInt(10000));
+//                    }catch (Exception e) {
+//                        e.getMessage();
+//                    }
+//
+//
+//                    System.out.println("sss="+j);
+//                }
+//            });
+//        }
+//        executor.execute(new Runnable() {
+//            public void run() {
+//                System.out.println("sss="+"333333");
+//            }
+//        });
+//        System.out.println("dssdsd");
+//        executor.shutdown();
+        String line = "[progress,20%,progress.check.liberty]";
+        System.out.println(line.indexOf(","));
+        System.out.println(line.indexOf("%,"));
+        System.out.println(line.substring(line.indexOf(",")+1,line.indexOf("%,")));
+    }
 }
