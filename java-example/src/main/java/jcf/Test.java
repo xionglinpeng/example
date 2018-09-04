fadcbb6a4de67bf72a6d551d97e5e5d2f8a9844a
xlp <xionglinpeng@163.com>
2018-09-04 16:49:39 +0800

    .

--- java-example/src/main/java/jcf/Test.java
+++ java-example/src/main/java/jcf/Test.java
@@ -1,11 +1,26 @@
 package jcf;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by haolw on 2018/6/7.
  */
 public class Test {
 
+
+
     public static void main(String[] args) {
-        System.out.println(1 << 3);
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>(1 << 4,0.75F,false);
+        map.put("1",1) ;
+        map.put("2",2) ;
+        map.put("3",3) ;
+        map.put("4",4) ;
+        map.put("5",5) ;
+        System.out.println(map.get("3"));
+//        System.out.println(map.get("2"));
+        map.forEach((k,v)->{
+            System.out.println(k+"=="+v);
+        });
     }
 }
