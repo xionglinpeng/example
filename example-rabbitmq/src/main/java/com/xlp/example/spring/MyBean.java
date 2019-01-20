6b807b3ea5d95f934cd4e2c4ede71d4babcb35b7
xlp <xionglinpeng@163.com>
2019-01-21 01:19:47 +0800

    .

--- example-rabbitmq/src/main/java/com/xlp/example/spring/MyBean.java
+++ example-rabbitmq/src/main/java/com/xlp/example/spring/MyBean.java
@@ -1,4 +1,29 @@
 package com.xlp.example.spring;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MyBean {
+
+    private AmqpTemplate amqpTemplate;
+    private AmqpAdmin amqpAdmin;
+
+    @Autowired
+    public MyBean(AmqpTemplate amqpTemplate, AmqpAdmin amqpAdmin) {
+        this.amqpTemplate = amqpTemplate;
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    public void test(){
+        amqpTemplate.convertAndSend("");
+    }
+
+    @RabbitListener(queues = "someQueue")
+    public void processMessage(String content){
+        //...
+    }
 }
